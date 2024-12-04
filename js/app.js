a250ed3d3af6b4e59aa28a89e98af8bbf4839ece
sadeen_hawash <sadeenghawash593@gmail.com>
2024-12-04 17:31:25 +0200

    build navigation using Append

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 /*------------------------------------ SHOW NAVIGATION BAR MENU ---------------------------------------*/
-const navBarMenuToggleBox = document.querySelector(".toggle-box");
+const navBarMenuItems = [
+  {
+    title: "Home",
+    link: "#",
+  },
+  {
+    title: "Performers",
+    link: "#performers",
+  },
+  {
+    title: "Shows",
+    link: "#shows",
+  },
+  {
+    title: "Contact",
+    link: "#contact",
+  },
+];
+
 const navBarMenu = document.querySelector(".nav-menu");
+
+navBarMenuItems.forEach((item) => {
+  const menuItem = document.createElement("li");
+  menuItem.innerHTML = `<a href="${item.link}">${item.title}</a>`;
+  navBarMenu.appendChild(menuItem);
+});
+
+/*------------------------------------ SHOW NAVIGATION BAR MENU ON TABLET AND MOBILE ---------------------------------------*/
+const navBarMenuToggleBox = document.querySelector(".toggle-box");
 
 navBarMenuToggleBox.addEventListener("click", () => {
   navBarMenu.style.display =
